63f3a648b2fdb0d99c865a111e20b591c2ba2aa0
aarthificial <aarthificial@users.noreply.github.com>
2023-07-30 13:15:18 +0000

    deploy: aarthificial-gamedev/typewriter@64ca558ce20825741efce51f1723360e23ea733a

--- public/main.js
+++ public/main.js
@@ -1,1 +1,10 @@
-export default {}
+﻿export default {
+  defaultTheme: 'dark',
+  iconLinks: [
+    {
+      icon: 'github',
+      href: 'https://github.com/aarthificial-gamedev/typewriter',
+      title: 'GitHub',
+    },
+  ],
+};
